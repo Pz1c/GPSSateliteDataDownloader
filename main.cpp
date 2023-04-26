206ba1104805ba76ed450614f636953305cb8bf2
Pz1C <viskgameua@gmail.com>
2023-04-26 11:25:00 +0300

    added parameters

--- main.cpp
+++ main.cpp
@@ -7,11 +7,20 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QNasaDownloader *qnd = new QNasaDownloader("pz1c", "Pz1cpz!c");
+    QStringList arg = a.arguments();
+    if (arg.count() < 5) {
+        qDebug() << "Wrong arguments count";
+        return 1;
+    }
+    QString DownloadLink = arg.at(1);
+    QString UserLogin = arg.at(2);
+    QString UserPassword = arg.at(3);
+    QString OutputFile = arg.at(4);
+
+    QNasaDownloader *qnd = new QNasaDownloader(UserLogin, UserPassword);
     QObject::connect(qnd, SIGNAL(finished(int)), qnd, SLOT(deleteLater()));
     QObject::connect(qnd, SIGNAL(finished(int)), &a, SLOT(exit(int)));
-    qnd->getFile("https://cddis.nasa.gov/archive/gnss/data/daily/2023/brdc/brdc1150.23n.gz",
-                "brdc1150.23n.gz");
+    qnd->getFile(DownloadLink, OutputFile);
 
     return a.exec();
 }
